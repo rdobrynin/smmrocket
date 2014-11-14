5c440896d69b34304a621b8e8dce1b932d89abe7
romdo <roman.d@brilliant-solutions.eu>
2014-11-14 15:11:53 +0200

    added flags

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-
+var pixelRatio = !!window.devicePixelRatio ? window.devicePixelRatio : 1;
+$(window).on("load", function() {
+    if (pixelRatio > 1) {
+//        $('#top-chest').attr('src', $('#top-chest').attr('src').replace(".png","@2x.png"));
+        $('body').addClass('retina');
+    }
+});
 
 $(function () {
 
